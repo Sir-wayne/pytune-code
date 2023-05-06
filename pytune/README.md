316827094f5642efddf75bc0ff747d52a68b1d6c
PNC-wayne <wayneadrianne.obeso@ctu.edu.ph>
2023-05-06 11:26:12 +0800

    uploading initals anad variable codes

---
+++ pytune/README.md
@@ -0,0 +1,3 @@
+#This is only for educational purposes
+
+Please learn as much we can. same like earnings so that we can earn more!
---
+++ pytune/variable.py
@@ -0,0 +1,35 @@
+# A variable is created the moment you first assign a value to it.
+# a = "hello"
+# b = "world"
+# c = a + b
+# print(c)
+
+#F string rule can only perform  concatenating
+# x=str("10")
+# y=int(10)
+# z=float(10.0)
+# print(f"{x}, {y}, {z}")
+
+#variable using reserve keyword type
+# a=input("pls input here :")
+# b_int=int(input("enter number"))
+# print(f"type{a}{b_int}")
+
+#assign multiple values you cannot modified due unpack tuples
+# x, y, z="chicharun", "bukarelyo", "ampaso"
+# print(x)
+# print(y)
+# print(z)
+
+#reverse in multiple value it will make one value also beware of sign(=)
+# x = y = z="chicharun"
+# print(x)
+# print(y)
+# print(z)
+
+#unpack tuples type of a varibale
+carcar_food=["chicharun", "bokarelyo", "ampao"]
+x, y, z = carcar_food
+print(x)
+print(y)
+print(z)
